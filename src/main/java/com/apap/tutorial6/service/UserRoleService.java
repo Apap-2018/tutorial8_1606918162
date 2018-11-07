63ae1171bd2937b2445918896e0810cc75c5af4d
Arief Fadli Wicaksono <arieffadliwicaksono@gmail.com>
2018-11-07 19:11:48 +0700

    latihan 1 done

---
+++ src/main/java/com/apap/tutorial6/service/UserRoleService.java
@@ -0,0 +1,9 @@
+package com.apap.tutorial6.service;
+
+import com.apap.tutorial6.model.UserRoleModel;
+
+public interface UserRoleService {
+	UserRoleModel addUser(UserRoleModel user);
+	public String encrypt(String password);
+
+}
